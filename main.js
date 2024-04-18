171dc7b3f2a98e923f35bb526ecbb4fa2eb299b6
leticia080 <126690016+leticia080@users.noreply.github.com>
2024-04-18 10:46:02 -0300

    18/04/2024

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ for(let i=0;i <botoes.length;i++){
         
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
+       
         botoes[i].classList.add("ativo");
-        textos[i].classList
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "Contagem regressiva";
